e00926e07494f9417401dd0554e51a7cf7edde30
meveo.admin <no-mail@meveo.com>
2023-03-25 01:07:31 +0100

    Add the cet java source file : Todo.javain the module : test

---
+++ facets/java/org/meveo/model/customEntities/Todo.java
@@ -0,0 +1,44 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.io.Serializable;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class Todo implements CustomEntity, Serializable {
+
+    public Todo() {
+    }
+
+    public Todo(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "Todo";
+    }
+}
