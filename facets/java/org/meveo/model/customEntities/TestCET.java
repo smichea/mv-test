871cecea5886302098ee6e0b1637668db37c67d2
meveo.admin <no-mail@meveo.com>
2023-03-28 01:18:47 +0200

    initial

---
+++ facets/java/org/meveo/model/customEntities/TestCET.java
@@ -0,0 +1,54 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.io.Serializable;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class TestCET implements CustomEntity, Serializable {
+
+    public TestCET() {
+    }
+
+    public TestCET(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    private String name;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "TestCET";
+    }
+}
